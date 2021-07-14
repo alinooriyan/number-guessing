34747006f2f63702af6c95eb0ed461894c5480c2
sahand2012p <sahandclevr45@gmail.com>
2021-07-14 02:22:40 -0700

    Corrected app for new secret number

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const createQuiz = require('./quizFile')
 const guess = createQuiz()
 
 guess.then((message) => {
-    console.log('Correct the number was 9 ' + message)
+    console.log('Correct the number was 5 ' + message)
 }).catch(message => {
     console.log('You failed please ' + message)
 })
