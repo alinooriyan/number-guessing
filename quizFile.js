e07071433478e92288e85d451a4ccd5dd37f529f
Sahand Pourjavad <sahndpourjavad@gmail.com>
2021-07-14 11:52:44 -0700

    Added quiz functionality

--- quizFile.js
+++ quizFile.js
@@ -4,5 +4,10 @@ const prompt = ps()
 const createQuiz = () => {
     return new Promise((resolve, reject) => {
         let number = parseInt(prompt('Enter a number to guess betweeen 1 and 10: '))
+        if (number === 9) {
+            resolve('Succsess')
+        } else {
+            reject('Try again.')
+        }
     })
 }
