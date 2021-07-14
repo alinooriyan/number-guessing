31ab234de9cb24f29503f54cb6e862c8b3dbd066
Sahand Pourjavad <sahndpourjavad@gmail.com>
2021-07-14 12:02:55 -0700

    fixed big bugs

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 const createQuiz = require('./quizFile')
 
-createQuiz().then((message) => console.log('Correct, the number was 9 ' + message)).catch((message) => console.log('You failed please ' + message))
+createQuiz.then((message) => console.log('Correct, the number was 9 ' + message)).catch((message) => console.log('You failed please ' + message))
