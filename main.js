eb85ecf7158900c61ab2bf433ecc8e26865cc9a6
Sahand Pourjavad <sahndpourjavad@gmail.com>
2021-07-14 12:00:12 -0700

    Finished createQuiz promise

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
 const createQuiz = require('./quizFile')
+
+createQuiz().then((message) => console.log('Correct, the number was 9 ' + message)).catch((message) => console.log('You failed please ' + message))
