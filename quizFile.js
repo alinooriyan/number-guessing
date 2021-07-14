2d6e8f3587f3bb6b442de3e6d016ad2e844c0b64
Sahand Pourjavad <sahndpourjavad@gmail.com>
2021-07-14 12:05:57 -0700

    fixed Quizfile

--- quizFile.js
+++ quizFile.js
@@ -1,15 +1,15 @@
-const ps = require('prompt-sync')
-const prompt = ps()
+const ps = require("prompt-sync");
+const prompt = ps();
 
 const createQuiz = () => {
     return new Promise((resolve, reject) => {
-        let number = parseInt(prompt('Enter a number to guess betweeen 1 and 10: '))
-        if (number === 9) {
-            resolve('Succsess')
+        let a = parseInt(prompt("Enter a number to guess Between 1 and 10: "));
+        if (a === 9) {
+            resolve("Success.");
         } else {
-            reject('Try again.')
+            reject("Try Again.");
         }
-    })
-}
+    });
+};
 
-exports = createQuiz
+module.exports = createQuiz;
